c3e9b20cb59e9bb01ca4e2f34e465e51a0ad14c5
Sonal Pachpute <sonalpachpute1999@gmail.com>
2021-04-19 10:52:52 +0530

    remove comments

--- src/Database_Connection.java
+++ src/Database_Connection.java
@@ -8,10 +8,8 @@ public class Database_Connection {
 	try{  
 		 Class.forName("com.mysql.jdbc.Driver");  
 		 Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/exam","root","");  
-		  //here exam is database name,username,password  
 		 System.out.println("Database is connected successfully");
 		 return con;
-		 //con.close();  
 		}catch(Exception e){ System.out.println(e);}
 	     return null;
         }
